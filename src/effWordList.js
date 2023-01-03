82db770b4ef8d4ca43e65fbfa217fb573aa2ce19
cesar-gz <cesarg7@csu.fullerton.edu>
2023-01-02 19:02:31 -0800

    pushing progress

--- src/effWordList.js
+++ src/effWordList.js
@@ -1,37 +1,6 @@
-//import {findWord} from './effWordList.js';
-
-export function eff(){
-    
-    var fiveDigits = diceRoll();
-
-    var passphrase = findWord2(fiveDigits)
-
-    return alert('Your randomized passphrase is: ', passphrase);
-  }
-
-function diceRoll(){
-    var randomNumber;
-    var text;
-    var finalNumber;
-
-    for(var i=0; i<5; i++){
-        if(i === 0){
-            randomNumber = Math.floor(Math.random() * 6) + 1;
-            text = randomNumber.toString();
-            finalNumber = text;
-            continue;
-        }
-
-        randomNumber = Math.floor(Math.random() * 6) + 1;
-        text = randomNumber.toString();
-        finalNumber = finalNumber + text;
-    }
-    return finalNumber;
-}
-
 // based off the eff_large_wordlist.txt"
 
-function findWord2(fiveDigits){ 
+export function findWord(number){ 
 
     var hashMap = new Map([
         [11111,	"abacus"],
@@ -7812,6 +7781,6 @@ function findWord2(fiveDigits){
         [66666,	"zoom"]
     ]);
 
-    let word = hashMap.get(fiveDigits);
+    let word = hashMap.get(number);
     return word;
 }
