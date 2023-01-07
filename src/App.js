41d830eebeb30ae66db2a91d4a2034ae9deaf160
cesar-gz <cesarg7@csu.fullerton.edu>
2023-01-07 12:07:19 -0800

    finished three more options.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {oldSchool} from './random.js';
+import {length12pw, length20pw, length32pw} from './random.js';
 import {eff} from './dice.js';
 
 function App() {
@@ -7,22 +7,34 @@ function App() {
     <div className="App">
         <h1>Hello welcome to my simple password generator!</h1>
 
-        <h2>Option 1 creates a randomized password that contains letters, digits, and special characters.
+        <p>Option 1 creates a randomized password that contains letters, digits, and special characters.
           It will allow you to choose an option between 12, 16, 20, or 32 character length.
-        </h2>
+        </p>
 
-        <h2>Option 2 will use the EFF Dice-Generated Passphrase to generate a randomized six-word passphrase
-        </h2>
+        <p>Option 2 will use the EFF Dice-Generated Passphrase to generate a randomized six-word passphrase
+        </p>
 
         <div>
-          <button onClick = {oldSchool}>
+          <button onClick = {length12pw}>
             Option 1
           </button>
         </div>
 
         <div>
-          <button onClick = {eff}>
+          <button onClick = {length20pw}>
             Option 2
+          </button>
+        </div>
+
+        <div>
+          <button onClick = {length32pw}>
+            Option 3
+          </button>
+        </div>
+
+        <div>
+          <button onClick = {eff}>
+            Option 4
           </button>
         </div>
     </div>
--- src/random.js
+++ src/random.js
@@ -1,4 +1,58 @@
-export function oldSchool(){
-    const password = 'This is your password:';
-    return alert(password);
+export function length12pw(){
+  var phrase = 'This is your 12 character password: ';
+  var password = generate(12);
+  return alert(phrase + password);
+}
+
+export function length20pw(){
+  var phrase = 'This is your 20 character password: ';
+  var password = generate(20);
+  return alert(phrase + password);
+}
+
+export function length32pw(){
+  var phrase = 'This is your 32 character password: ';
+  var password = generate(32);
+  return alert(phrase + password);
+}
+
+function generate(number){
+  var password;
+  var randomNumber;
+
+  // 1 = abcs, 2 = ABCs, 3 = digits, 4 = special characters
+  randomNumber = Math.floor(Math.random() * (4-1)+1);
+  password = inputChar(randomNumber);
+
+  for(var i = 0; i<number-1; i++){
+    randomNumber = Math.floor(Math.random() * (4-1+1)+1);
+    password = password + inputChar(randomNumber);
   }
+
+  return password;
+}
+
+function inputChar(randomNumber){
+
+  let asciiLowercase = 'abcdefghijklmnopqrstuvwxyz';
+  let asciiUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let digits = '0123456789';
+  let specialChars = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+  if(randomNumber === 1){
+    let randNum = Math.floor(Math.random() * (26-1)+1);
+    return asciiLowercase.charAt(randNum);
+  }
+  if(randomNumber === 2){
+    let randNum = Math.floor(Math.random() * (26-1)+1);
+    return asciiUppercase.charAt(randNum);
+  }
+  if(randomNumber === 3){
+    let randNum = Math.floor(Math.random() * (10-1)+1);
+    return digits.charAt(randNum)
+  }
+  if(randomNumber === 4){
+    let randNum = Math.floor(Math.random() * (30-1)+1);
+    return specialChars.charAt(randNum)
+  }
+}
