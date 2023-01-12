db1949fd608c1b0840795eb87d087a6a8d8b97f1
cesar-gz <cesarg7@csu.fullerton.edu>
2023-01-11 20:38:23 -0800

    added css styling

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
 
   return (
     <div className="App">
+        
+        <div class= "typewriter">
         <h1>Hello welcome to my simple password generator!</h1>
+        </div>
 
         <p>The first three buttons creates a randomized password.</p>
-        <p>Each password contains letters, digits, and special characters.</p>
+        <p>Each password contains letters, digits, and special characters in a random order.</p>
         <p>You can choose between 12, 20, or 32 characters long.</p>
+
 
         <div>
           <button onClick = {length12pw}>
@@ -32,7 +36,7 @@ function App() {
           </button>
         </div>
 
-        <p>This button will create a EFF Dice-Generated Passphrase.</p>
+        <p>The button below will create a EFF Dice-Generated Passphrase.</p>
         <p>Its just six random words combined to make one password phrase.</p>
         <p>This is recommended by the <a href={url}>Electric Frontier Foundation</a>.</p>
 
@@ -41,6 +45,9 @@ function App() {
             EFF passphrase
           </button>
         </div>
+        
+        <p>Made by Cesar Gutierrez.</p>
+
 
     </div>
   );
